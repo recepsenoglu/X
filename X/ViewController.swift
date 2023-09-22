4c958e862a5d25b1d8422e5ac642b4af37f47979
Recep Oğuzhan Şenoğlu <recep.senoglu.34@gmail.com>
2023-09-22 10:23:07 +0300

    Initial Commit

---
+++ X/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  X
+//
+//  Created by Recep Oğuzhan Şenoğlu on 22.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
