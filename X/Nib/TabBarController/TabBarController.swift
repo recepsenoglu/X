5378029bc783864c0a57185e923db1748e4cc3b7
Recep Oğuzhan Şenoğlu <recep.senoglu.34@gmail.com>
2023-11-17 17:07:34 +0300

    Custom tabbar controller

--- X/Nib/TabBarController/TabBarController.swift
+++ X/Nib/TabBarController/TabBarController.swift
@@ -8,22 +8,56 @@
 import UIKit
 
 class TabBarController: UITabBarController {
-
+    
+    // MARK: - Outlets
+    
+    @IBOutlet private var customTabBarView: UIView!
+    @IBOutlet private weak var homeButton: UIButton!
+    @IBOutlet private weak var messagesButton: UIButton!
+    
+    // MARK: - Variables
+    
+    private var homeButtonImage: UIImage? = UIImage(systemName: "house")?.withTintColor(.black, renderingMode: .alwaysOriginal)
+    private var messagesButtonImage: UIImage? = UIImage(systemName: "envelope")?.withTintColor(.black, renderingMode: .alwaysOriginal)
+    private var homeButtonSelectedImage: UIImage? = UIImage(systemName: "house.fill")?.withTintColor(.black, renderingMode: .alwaysOriginal)
+    private var messagesButtonSelectedImage: UIImage? = UIImage(systemName: "envelope.fill")?.withTintColor(.black, renderingMode: .alwaysOriginal)
+    
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addCustomTabBarViewProgrammatically()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Functions
+    
+    private func addCustomTabBarViewProgrammatically() {
+        view.addSubview(customTabBarView)
+        customTabBarView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            customTabBarView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 40),
+            customTabBarView.heightAnchor.constraint(equalToConstant: 100),
+            customTabBarView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            customTabBarView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
     }
-    */
-
+    
+    // MARK: - Actions
+    
+    @IBAction private func changeTab_TUI(_ sender: Any) {
+        homeButton.setImage(homeButtonImage, for: .normal)
+        messagesButton.setImage(messagesButtonImage, for: .normal)
+        let index = (sender as! UIButton).tag
+        switch index {
+        case 0:
+            homeButton.setImage(homeButtonSelectedImage, for: .normal)
+        case 1:
+            messagesButton.setImage(messagesButtonSelectedImage, for: .normal)
+        default:
+            homeButton.setImage(homeButtonSelectedImage, for: .normal)
+        }
+        selectedIndex = index
+    }
+    
 }
